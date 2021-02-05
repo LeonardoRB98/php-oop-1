a135f3ef62382711a621e164fd1a3d71990f901b
Leonardo <leonardo.ranaldo.b@gmail.com>
2021-02-05 18:00:07 +0100

    istanze create

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+    class Book {
+
+      //attributi
+      public $title;
+      public $author;
+      public $pubblicationDate;
+      public $publishingHouse;
+      public $price;
+
+      // construct
+      function __construct($titleCo, $authorCo, $pubblicationDateCo, $publishingHouseCo, $priceCo) {
+          $this->$title = $titleCo;
+          $this->$author = $authorCo; 
+          $this->$pubblicationDate = $pubblicationDateCo; 
+          $this->$publishingHouse = $publishingHouseCo;  
+          $this->$price = $priceCo; 
+        }
+        
+    }
+
+    $book1 = new Book("Harry Potter and the Philosophers Stone", "J.K. Rowling", "1997", "Gryffindor Edition", "20");
+    var_dump($book1);
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
